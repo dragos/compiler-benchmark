cc805e4661dc9b9f6c47527bcbee259de5112243
Iulian Dragos <jaguarul@gmail.com>
2017-11-27 16:41:01 +0100

    Update to 0.9.9

--- compilation/src/main/hydra/scala/tools/benchmark/BenchmarkDriver.scala
+++ compilation/src/main/hydra/scala/tools/benchmark/BenchmarkDriver.scala
@@ -5,34 +5,12 @@ import scala.tools.nsc.CompilerCommand
 import scala.tools.nsc.Global
 import scala.tools.nsc.reporters.Reporter
 
-import com.triplequote.hydra.Hydra
-import com.triplequote.hydra.HydraSettings
-import com.triplequote.hydra.compiler.HydraGlobal
-import com.triplequote.hydra.compiler.classpath.ParsedLogicalPackage
+import com.triplequote.hydra.Main
 
 trait BenchmarkDriver extends BaseBenchmarkDriver {
-  // All this code is copy-pasted from Hydra because Hydra.main calls System.exit (needed for Maven support)
-  // and we don't have another entry point that's convenient to use.
   def compileImpl(): Unit = {
-    val scalaSettings = new HydraSettings(Console.err.println)
-    val command = new CompilerCommand(allArgs.toList, scalaSettings)
-
-    val exitCode = Hydra.validateArguments(command) match {
-      case Left(Hydra.CompilerSettingsError(msg)) =>
-        scalaSettings.errorFn(msg)
-        1
-      case Right(sources) =>
-        val cpStructure = ParsedLogicalPackage.collectLogicalPackages(scalaSettings)
-        val newGlobal: Reporter => HydraGlobal = (reporter: Reporter) => new Global(scalaSettings, reporter) with HydraGlobal {
-          override lazy val classPathStructure = cpStructure
-        }
-        val newRun = (global: HydraGlobal) => new global.Run
-
-        Hydra.compile(sources, command, newGlobal, newRun) match {
-          case Left(_) => 1
-          case Right(hasErrors) => if (hasErrors) 1 else 0
-        }
-    }
+    // this works starting with Hydra 0.9.9
+    val exitCode = Main.compile(allArgs.toArray)
 
     assert(exitCode == 0, "Errors during compilation")
   }
